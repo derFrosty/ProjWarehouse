d22430fc9829ee5519d3979adbfd7268d0069b75
Frederico Gomes <frederico.94@hotmail.com>
2020-04-21 14:15:46 +0100

    executeActions e isGoal do WarehouseProblemForSearch

--- src/warehouse/WarehouseProblemForSearch.java
+++ src/warehouse/WarehouseProblemForSearch.java
@@ -30,12 +30,19 @@ public class WarehouseProblemForSearch<S extends WarehouseState> extends Problem
 
     @Override
     public List<S> executeActions(S state) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        List<S> successors = new LinkedList<>();
+
+        for (Action action: actions) {
+            if (action.isValid(state)){
+                S successor = (S) state.clone();
+                action.execute(successor);
+                successors.add(successor);
+            }
+        }
+        return successors;
     }
 
     public boolean isGoal(S state) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return state.getLineAgent()==getGoalPosition().getLine() && state.getColumnAgent()==getGoalPosition().getColumn();
     }
 }
