c66b02b0cbba45b582f7141805ee136935360abd
Frederico Gomes <frederico.94@hotmail.com>
2020-04-21 14:10:48 +0100

    construtor do WarehouseProblemForSearch com as actions

--- src/warehouse/WarehouseProblemForSearch.java
+++ src/warehouse/WarehouseProblemForSearch.java
@@ -1,17 +1,27 @@
 package warehouse;
 
+import agentSearch.Action;
 import agentSearch.Problem;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class WarehouseProblemForSearch<S extends WarehouseState> extends Problem<S> {
 
     private Cell goalPosition;
+    private List<Action> actions;
 
     public WarehouseProblemForSearch(S initialWarehouseState, Cell goalPosition) {
         super(initialWarehouseState);
-
         this.goalPosition = goalPosition;
+
+        //lista de ações
+        actions = new LinkedList<Action>(){{
+            add(new ActionUp());
+            add(new ActionLeft());
+            add(new ActionDown());
+            add(new ActionRight());
+        }};
     }
 
     public Cell getGoalPosition() {
