0a8228e81be946d6f722426dc56e6823a4451ec8
Frederico Gomes <frederico.94@hotmail.com>
2020-04-22 14:35:28 +0100

    WarehouseProblemForGa (com a professora)

--- src/warehouse/WarehouseProblemForGA.java
+++ src/warehouse/WarehouseProblemForGA.java
@@ -2,27 +2,55 @@ package warehouse;
 
 import ga.Problem;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class WarehouseProblemForGA implements Problem<WarehouseIndividual> {
 
-    //TODO this class might require the definition of additional methods and/or attributes
+    private LinkedList<Cell> shelves;
+    private Cell cellAgent;
+    private Cell exit;
+    private ArrayList<Request> requests;
+    private int numProducts;
+    private LinkedList<Pair> pairs;
 
     public WarehouseProblemForGA(WarehouseAgentSearch agentSearch) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.shelves = agentSearch.getShelves();
+        this.cellAgent = agentSearch.getCellAgent();
+        this.exit = agentSearch.getExit();
+        this.requests=agentSearch.getRequests();
+        this.numProducts = agentSearch.getNumProducts();
+        this.pairs =  agentSearch.getPairs();
+
+    }
+
+    public LinkedList<Cell> getShelves() {
+        return shelves;
+    }
+
+    public Cell getCellAgent() {
+        return cellAgent;
+    }
+
+    public Cell getExit() {
+        return exit;
+    }
+
+    public ArrayList<Request> getRequests() {
+        return requests;
+    }
+
+    public int getNumProducts() {
+        return numProducts;
+    }
+
+    public LinkedList<Pair> getPairs() {
+        return pairs;
     }
 
     @Override
     public WarehouseIndividual getNewIndividual() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
-    }
-
-    @Override
-    public String toString() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return new WarehouseIndividual(this,shelves.size());
     }
 
 }
