14208199f7d8537b0097d280e418e23d38de35db
Siilvaaz <jmvs98@gmail.com>
2020-04-28 14:06:07 +0100

    Implementação da mutation2 - Inversion

--- src/ga/geneticOperators/Mutation2.java
+++ src/ga/geneticOperators/Mutation2.java
@@ -1,5 +1,6 @@
 package ga.geneticOperators;
 
+import ga.GeneticAlgorithm;
 import ga.IntVectorIndividual;
 import ga.Problem;
 
@@ -11,13 +12,29 @@ public class Mutation2<I extends IntVectorIndividual, P extends Problem<I>> exte
 
     @Override
     public void mutate(I ind) {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int cut1 = GeneticAlgorithm.random.nextInt(ind.getNumGenes());
+        int cut2;
+        do {
+            cut2 = GeneticAlgorithm.random.nextInt(ind.getNumGenes());
+        }while (cut1==cut2);
+        if (cut1 > cut2) {
+            int aux = cut1;
+            cut1 = cut2;
+            cut2 = aux;
+        }
+        for(int i = cut2; i > cut1 ; i--) {
+            int aux = ind.getGene(i);
+            ind.setGene(i, ind.getGene(cut1));
+            ind.setGene(cut1, ind.getGene(aux));
+
+            cut1++;
+
+        }
+
     }
 
     @Override
     public String toString() {
-        //TODO
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return "Inversion";
     }
 }
